92153f779f6b396ac77c16334069f8d64115ba79
Mikisho <mikiye.82@gmail.com>
2017-12-27 16:47:37 -0500

    all

--- components/TextInput.js
+++ components/TextInput.js
@@ -10,10 +10,16 @@ class TextInput extends Component {
     }
   }
 
+  deleteLetter() {
+    this.setState({
+      inputText: this.state.inputText.substring(0, this.state.inputText.length - 1)
+    });
+  }
+
   handleChange(event) {
     this.setState({
       inputText: event.target.value
-    })
+    });
   }
 
   render() {
@@ -25,7 +31,10 @@ class TextInput extends Component {
           value={this.state.inputText}
           onChange={this.handleChange.bind(this)}
         />
-        <TextDisplay text={this.state.inputText}/>
+        <TextDisplay 
+          text={this.state.inputText}
+          deleteLetter = {this.deleteLetter.bind(this)}
+        />
       </div>
     )
   }
